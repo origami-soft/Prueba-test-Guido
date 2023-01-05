00a463c8313271df09eb1f11af50ff32de4746f2
guido <guido@MacBook-Pro-de-ITECNIS.local>
2023-01-05 15:56:26 -0300

    [Prueba Carpeta]

---
+++ prueba_guido/models/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class crm_lead(models.Model):
+    _inherit = 'crm.lead'
+
+    prueba = fields.Char(string='Prueba')
+    selecction = fields.Char(string='Selection')
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    prueba = fields.Char(string='Prueba')
+
