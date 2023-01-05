16d18b4552a5863366ff50be3849424f1d81b6b5
guido <guido@MacBook-Pro-de-ITECNIS.local>
2023-01-05 16:08:57 -0300

    Comento

--- prueba_guido/models/models.py
+++ prueba_guido/models/models.py
@@ -3,15 +3,15 @@
 from odoo import models, fields, api
 
 
-class crm_lead(models.Model):
-    _inherit = 'crm.lead'
+# class crm_lead(models.Model):
+#     _inherit = 'crm.lead'
 
-    prueba = fields.Char(string='Prueba')
-    selecction = fields.Char(string='Selection')
+#     prueba = fields.Char(string='Prueba')
+#     selecction = fields.Char(string='Selection')
 
 
-class sale_order(models.Model):
-    _inherit = 'sale.order'
+# class sale_order(models.Model):
+#     _inherit = 'sale.order'
 
-    prueba = fields.Char(string='Prueba')
+#     prueba = fields.Char(string='Prueba')
 
