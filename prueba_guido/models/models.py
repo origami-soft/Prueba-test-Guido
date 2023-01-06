4b7577382e8b8b8cefc6fc27190d2d52c1de9aa8
guido <guido@MacBook-Pro-de-ITECNIS.local>
2023-01-06 15:53:17 -0300

    []

--- prueba_guido/models/models.py
+++ prueba_guido/models/models.py
@@ -2,21 +2,21 @@
 
 from odoo import models, fields, api
 
-# class prueba_prueba(models.Model):
-#     _name = 'prueba.prueba'
+class prueba_prueba(models.Model):
+    _name = 'prueba.prueba'
 
-#     name = fields.Char(string='Nombre')
-
-
-# class crm_lead(models.Model):
-#     _inherit = 'crm.lead'
-
-#     prueba = fields.Char(string='Prueba')
-#     selecction = fields.Char(string='Selection')
+    name = fields.Char(string='Nombre')
 
 
-# class sale_order(models.Model):
-#     _inherit = 'sale.order'
+class crm_lead(models.Model):
+    _inherit = 'crm.lead'
 
-#     prueba = fields.Char(string='Prueba')
+    prueba = fields.Char(string='Prueba')
+    selecction = fields.Char(string='Selection')
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    prueba = fields.Char(string='Prueba')
 
