9b0b972ecd0bb207b669fc885c1b7553e64f9398
guido <guido@MacBook-Pro-de-ITECNIS.local>
2023-01-10 15:59:19 -0300

    []

--- prueba_guido/models/models.py
+++ prueba_guido/models/models.py
@@ -8,10 +8,6 @@ class crm_lead(models.Model):
     prueba = fields.Char(string='Prueba')
     selecction = fields.Char(string='Selection')
 
-
-    
-
 class sale_order(models.Model):
     _inherit = 'sale.order'
-
     prueba = fields.Char(string='SALES')
